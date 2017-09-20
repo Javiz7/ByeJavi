4eccf50bf4ec475d7617ac3ed957f0c574bd22c2
Javiz7 <31923944+Javiz7@users.noreply.github.com>
2017-09-20 12:11:45 +0200

    *.java

---
+++ Bye.java
@@ -0,0 +1,8 @@
+public class Bye {
+	public static void main(String[] args) {
+		System.out.println("#############");
+		for (int i=0; i<3; i++) {
+			System.out.println("Bye Javi..."+i);
+		}
+	}
+}
